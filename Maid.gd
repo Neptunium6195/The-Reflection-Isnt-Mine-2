4fc321384ed3953e4256c2225158932d36535b30
Neptunium6195 <seratesarah@gmail.com>
2025-12-15 15:17:51 -0800

    minigames

--- Maid.gd
+++ Maid.gd
@@ -4,9 +4,13 @@ extends Node
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	Dialogic.start("timeline1")
-	pass # Replace with function body.
+	Dialogic.timeline_ended.connect(_on_dialogue_finished)
+	Dialogic.start("timeline")
 
 
+func _on_dialogue_finished():
+	get_tree().change_scene_to_file("res://books.tscn")
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
