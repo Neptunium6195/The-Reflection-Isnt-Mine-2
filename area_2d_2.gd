84ac7c788e7772f1d81fe82877b3c59213166f8b
ctln-d <caitlindu10@gmail.com>
2025-12-18 14:08:01 -0500

    started mirror cleaning minigame and finished day 2 timelines

--- area_2d_2.gd
+++ area_2d_2.gd
@@ -16,3 +16,7 @@ func _on_area_entered(area: Area2D) -> void:
 	set_deferred("monitorable", false)
 	pass
 	
+
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	pass # Replace with function body.
